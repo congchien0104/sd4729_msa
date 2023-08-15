39cc47c6be77f972bf3df27e9edb11526625613a
Chien Neymar <chienphungc@nashtechglobal.com>
2023-08-15 10:13:39 +0700

    add ecr for jenkins

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 void call(Map pipelineParams) {
     String name = 'backend'
-    String ecrUrl = '480566855108.dkr.ecr.us-east-1.amazonaws.com'
+    String ecrUrl = '893473272543.dkr.ecr.us-east-1.amazonaws.com'
     String awsRegion = 'us-east-1'
     String clusterName = 'eks-demo'
     pipeline {
@@ -30,17 +30,17 @@ void call(Map pipelineParams) {
                 }
             }
 
-            // stage('Build Docker Image') {
-            //     steps {
-            //         // Build Docker Image for Application
-            //         withAWS(credentials: 'aws-credentials', region: "${awsRegion}") {
-            //             sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrUrl}"
-            //             sh "docker build -t ${name} ."
-            //             sh "docker tag ${name}:latest ${ecrUrl}/${name}:latest"
-            //             sh "docker push ${ecrUrl}/${name}:latest" 
-            //         }
-            //     }
-            // }
+            stage('Build Docker Image') {
+                steps {
+                    // Build Docker Image for Application
+                    withAWS(credentials: 'aws-credentials', region: "${awsRegion}") {
+                        sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrUrl}"
+                        sh "docker build -t ${name} ."
+                        sh "docker tag ${name}:latest ${ecrUrl}/${name}:latest"
+                        sh "docker push ${ecrUrl}/${name}:latest" 
+                    }
+                }
+            }
 
             // stage('Deploy') {
             //     steps {
